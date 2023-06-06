6e7761d54310520431f862c785355020c94d2bd6
Harikrishna Makwana <90233095+harikrishna-makwana@users.noreply.github.com>
2023-06-06 11:17:10 +0530

    Text field, btn, image

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_for_it/pages/home_page.dart';
 import 'package:go_for_it/pages/login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(Myapp());
@@ -16,7 +17,10 @@ class Myapp extends StatelessWidget {
     return MaterialApp(
       //theme modes code
       themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        fontFamily: GoogleFonts.lato().fontFamily,
+      ),
       darkTheme: ThemeData(brightness: Brightness.dark),
       //Routes
       initialRoute: "/login",
